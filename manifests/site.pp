85531c90ab7020a7d28aa9f6c8a2b46b01e67942
root <root@chandu8114.mylabserver.com>
2018-01-09 18:14:28 +0000

    the is external enc node decleration

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,10 +23,6 @@ File { backup => false }
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-$test_top_scope = "top scope"
-#node 'chandu8115.mylabserver.com' {
- #notify { 'test-masessage from site.pp': }
-#}
 
 node default {
   hiera_include('classes')
